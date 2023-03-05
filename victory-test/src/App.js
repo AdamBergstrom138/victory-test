474dae3c4a1da34a75c9dd81952dfabbf3563755
Adam Bergstrom <abx1138@gmail.com>
2023-03-05 10:02:13 -0600

    created bar graph

---
+++ victory-test/src/App.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+
+const data2012 = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
+
+const data2013 = [
+  {quarter: 1, earnings: 15000},
+  {quarter: 2, earnings: 12500},
+  {quarter: 3, earnings: 19500},
+  {quarter: 4, earnings: 13000}
+];
+
+const data2014 = [
+  {quarter: 1, earnings: 11500},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 20000},
+  {quarter: 4, earnings: 15500}
+];
+
+const data2015 = [
+  {quarter: 1, earnings: 18000},
+  {quarter: 2, earnings: 13250},
+  {quarter: 3, earnings: 15000},
+  {quarter: 4, earnings: 12000}
+];
+
+const characterData = [
+  { strength: 1, intelligence: 250, luck: 1, stealth: 40, charisma: 50 },
+  { strength: 2, intelligence: 300, luck: 2, stealth: 80, charisma: 90 },
+  { strength: 5, intelligence: 225, luck: 3, stealth: 60, charisma: 120 }
+];
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Victory Graph Test</h1>
+      <VictoryChart
+        domainPadding={20}
+        theme={VictoryTheme.material}
+      >
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={(x) => (`$${x / 1000}k`)}
+        />
+        <VictoryStack>
+          <VictoryBar
+            data={data2012}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2013}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2014}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2015}
+            x="quarter"
+            y="earnings"
+          />
+        </VictoryStack>
+      </VictoryChart>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// class Main extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Victory Tutorial</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// const app = document.getElementById('app');
+// ReactDOM.render(<Main />, app);
